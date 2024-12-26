54b35323cf1eccd8d09bb0a053e40f1884ef13e6
Xalil404 <kareemxalil@gmail.com>
2024-12-26 18:15:55 +0000

    Added home page and contact page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
+import NotFound from './components/NotFound';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Contact from './components/Contact';
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <Navbar /> {/* Add the Navbar here !! */}
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} /> {/* Fallback route for 404 Page */}
+              <Route path="/contact" element={<Contact />} />
+          </Routes>
+      </Router>
   );
-}
+};
 
 export default App;
